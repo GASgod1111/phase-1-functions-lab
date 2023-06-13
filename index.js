87e16ec17f93487d24ecae36af88bd2f4c4822a6
GASgod1111 <reagan.peter@student.moringaschool.com>
2023-06-13 12:39:33 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(somevalue)  {
+    const HqLocation=42;
+    return pickupLocation -HqLocation;
+}
+console.log(substraction(50,42));
+
+function distanceFromInFeet(someValue) {
+    
+    const feetperBlock=264;
+    return blocks*feetperBlock;
+
+
+}
+ function distanceTraveledInFeet (start,end) {
+    const blocks=math.abs(endBlocks - startBlocks);
+    const feetPerBlock=264;
+    return blocks *feetPerBlock;
+ 
+ }
+ function calculatesFarePrice(start, destination){
+
+ n
+ }
